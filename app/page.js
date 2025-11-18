bef113150999fdaefba64bba042c19745cf8d349
Nagesh-chegondi <147309507+Nagesh-chegondi@users.noreply.github.com>
2025-11-18 19:59:25 +0530

    Update page.js

--- app/page.js
+++ app/page.js
@@ -3,8 +3,8 @@ import { redirect } from "next/navigation";
 
 export default async function HomePage() {
 
-  const token  = await cookies().get("session_token").value;
-  console.log(token)
+   const sessionCookie = cookies().get("session_token");
+  const token = sessionCookie?.value
 
   if (!token) {
     redirect("/landing");
